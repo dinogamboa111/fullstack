4ddc5cf161e2a4312534f6225218eb7330c72ec9
dinogamboa111 <din.gamboa@duocuc.cl>
2025-04-28 14:24:47 -0400

    1
    1

---
+++ ProyectoConMS/usuario-ms/src/test/java/cl/fullstack/usuario_ms/UsuarioMsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.fullstack.usuario_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
