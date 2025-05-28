284c9c9b9673333d77b2cdc5547ff3f12c2708bb
dinogamboa111 <din.gamboa@duocuc.cl>
2025-05-27 21:50:04 -0400

    11
    11

--- ProyectoConMS/centro-distribucion-ms/src/main/java/cl/fullstack/centro_distribucion_ms/repository/ComunaCubiertaRepository.java
+++ ProyectoConMS/centro-distribucion-ms/src/main/java/cl/fullstack/centro_distribucion_ms/repository/ComunaCubiertaRepository.java
@@ -1,15 +1,16 @@
 package cl.fullstack.centro_distribucion_ms.repository;
 
+
 import cl.fullstack.centro_distribucion_ms.entity.ComunaCubiertaEntity;
-import cl.fullstack.centro_distribucion_ms.entity.ComunaCubiertaPKCompuesta;
+import cl.fullstack.centro_distribucion_ms.entity.ComunaCubiertaPKcompuesta;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository // indica que esta interfaz es un repositorio de datos
-public interface ComunaCubiertaRepository extends JpaRepository<ComunaCubiertaEntity,ComunaCubiertaPKCompuesta > {  
+@Repository // indica que esta interfaz es un repositorio de dato
+public interface ComunaCubiertaRepository extends JpaRepository<ComunaCubiertaEntity,ComunaCubiertaPKcompuesta> {  
     // repositorio para operaciones CRUD sobre comuna cubierta usando llave compuesta
 
  //////////////////////////////////////////////////////////////////////////   
