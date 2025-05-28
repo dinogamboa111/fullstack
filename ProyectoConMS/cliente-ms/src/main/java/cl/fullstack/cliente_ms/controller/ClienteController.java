0bba06aab007caff67135bc7c9184ebd45189a7e
dinogamboa111 <din.gamboa@duocuc.cl>
2025-05-28 10:12:47 -0400

    actualizacion valid
    se elimina vallid de controler

--- ProyectoConMS/cliente-ms/src/main/java/cl/fullstack/cliente_ms/controller/ClienteController.java
+++ ProyectoConMS/cliente-ms/src/main/java/cl/fullstack/cliente_ms/controller/ClienteController.java
@@ -1,12 +1,12 @@
 package cl.fullstack.cliente_ms.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,9 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cl.fullstack.cliente_ms.dto.ClienteDTO;
-import cl.fullstack.cliente_ms.exception.DatosInvalidosException;
+
 import cl.fullstack.cliente_ms.service.IClienteService;
-import jakarta.validation.Valid;
+
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -29,18 +29,31 @@ public class ClienteController {
     private IClienteService clienteService;
 
     @PostMapping
-    public ResponseEntity<ClienteDTO> crear(@Valid @RequestBody ClienteDTO dto, //el valid activa los bean validation NotNull Email etc
-                                             BindingResult result) { 
-        if (result.hasErrors()) { // si capta un error de validacion procede con el if
-            String errores = result.getFieldErrors().stream() //obtiene todos los errores y los pasa a stream
-                    .map(e -> e.getField() + ": " + e.getDefaultMessage()) // aqui los convierte a un string, ejemplo : email: debe ser correo valido
-                    .collect(Collectors.joining("; ")); // une cada error  que esta y te arma un solo string , los separa con ;
-            throw new DatosInvalidosException(errores); //lanza mi exception, pueden ver la exception declarada en GlobalException y tiene su propia clase igual que lleva el "metodo"
-        }
+    public ResponseEntity<ClienteDTO> crear(/* @Valid */ @RequestBody ClienteDTO dto /* , BindingResult result */) {
 
-        ClienteDTO creado = clienteService.crearCliente(dto); // si no hay errores desp de todo, llama al servicio "crearCliente" quien lo guarda en la bbdd, pueden ver el metodo que contiene validaciones propias en SERVICE
-        return ResponseEntity.status(HttpStatus.CREATED).body(creado); //retorna un gttp 201 que significa que se creo, y 
-                                                                        // pasa el dto creado en el body, osea en postman saldra el body que se logro crear, igual si lo prueban se inserta en la bbdd directo
+        /*
+         * if (result.hasErrors()) { // si capta un error de validacion procede con el
+         * if
+         * String errores = result.getFieldErrors().stream() //obtiene todos los errores
+         * y los pasa a stream
+         * .map(e -> e.getField() + ": " + e.getDefaultMessage()) // aqui los convierte
+         * a un string, ejemplo : email: debe ser correo valido
+         * .collect(Collectors.joining("; ")); // une cada error que esta y te arma un
+         * solo string , los separa con ;
+         * throw new DatosInvalidosException(errores); //lanza mi exception, pueden ver
+         * la exception declarada en GlobalException y tiene su propia clase igual que
+         * lleva el "metodo"
+         * }
+         */
+
+        ClienteDTO creado = clienteService.crearCliente(dto); // si no hay errores desp de todo, llama al servicio
+                                                              // "crearCliente" quien lo guarda en la bbdd, pueden ver
+                                                              // el metodo que contiene validaciones propias en SERVICE
+        return ResponseEntity.status(HttpStatus.CREATED).body(creado); // retorna un gttp 201 que significa que se creo,
+                                                                       // y
+                                                                       // pasa el dto creado en el body, osea en postman
+                                                                       // saldra el body que se logro crear, igual si lo
+                                                                       // prueban se inserta en la bbdd directo
     }
 
     // @PostMapping
@@ -75,41 +88,47 @@ public class ClienteController {
 
     @PutMapping("/{rut}")
     public ResponseEntity<ClienteDTO> actualizar(@PathVariable int rut,
-            @Valid @RequestBody ClienteDTO dto,
-            BindingResult result) {
-        if (result.hasErrors()) {
-            String errores = result.getFieldErrors().stream() // obtiene todos los errores
-                    .map(e -> e.getField() + ": " + e.getDefaultMessage()) // transforma cada error en un string ej:
-                                                                           // email: no puede estar vacio, esto segun
-                                                                           // los parametros que usamos como notNull
-                                                                           // Email etc
-                    .collect(Collectors.joining("; ")); // une todos los errores en un solo string separado por el ;
-            throw new DatosInvalidosException(errores); // lanza nuestro exception programada en ese metodo
-        }
+            @RequestBody ClienteDTO dto) {
 
-        ClienteDTO actualizado = clienteService.actualizarCliente(rut, dto); // SI NO EXISTIERON ERRORES DE VALIDACION,
-                                                                             // osea se encontro el rut, se llama al
-                                                                             // SERVICE para actualizar,
-                                                                             // esta actualizacion se guarda finalmente
-                                                                             // en la bbdd, el save se ejecuta en el
-                                                                             // metodo declarado en el SERVICE
-                                                                             // **RECORDAR QUE EL METODO EN SERVICE
-                                                                             // TIENE TAMBIEN
-                                                                             // CONTROL DE ERRORES, COMO PK DUPLICADA, O
-                                                                             // MAIL DUPLICADO.
+        ClienteDTO actualizado = clienteService.actualizarCliente(rut, dto);
+        // se llama al service para actualizar
+        // Todas las validacione estan  manejadas dentro del service.
 
-        return ResponseEntity.ok(actualizado); // aqui nos devuelve el exito con un codigo 200 http
+        return ResponseEntity.ok(actualizado); // Devuelve HTTP 200 con el cliente actualizado en el body.
     }
+
     // @PutMapping("/{rut}")
-    // public ResponseEntity<ClienteDTO> actualizar(@PathVariable int rut, @Valid
-    // @RequestBody ClienteDTO dto,
+    // public ResponseEntity<ClienteDTO> actualizar(@PathVariable int rut,
+    // @Valid @RequestBody ClienteDTO dto,
     // BindingResult result) {
     // if (result.hasErrors()) {
-    // // Aquí manejas los errores de validación
-    // return ResponseEntity.badRequest().body(null); // O puedes devolver detalles
-    // de los errores
+    // String errores = result.getFieldErrors().stream() // obtiene todos los
+    // errores
+    // .map(e -> e.getField() + ": " + e.getDefaultMessage()) // transforma cada
+    // error en un string ej:
+    // // email: no puede estar vacio, esto segun
+    // // los parametros que usamos como notNull
+    // // Email etc
+    // .collect(Collectors.joining("; ")); // une todos los errores en un solo
+    // string separado por el ;
+    // throw new DatosInvalidosException(errores); // lanza nuestro exception
+    // programada en ese metodo
     // }
-    // return ResponseEntity.ok(clienteService.actualizarCliente(rut, dto));
+
+    // ClienteDTO actualizado = clienteService.actualizarCliente(rut, dto); // SI NO
+    // EXISTIERON ERRORES DE VALIDACION,
+    // // osea se encontro el rut, se llama al
+    // // SERVICE para actualizar,
+    // // esta actualizacion se guarda finalmente
+    // // en la bbdd, el save se ejecuta en el
+    // // metodo declarado en el SERVICE
+    // // **RECORDAR QUE EL METODO EN SERVICE
+    // // TIENE TAMBIEN
+    // // CONTROL DE ERRORES, COMO PK DUPLICADA, O
+    // // MAIL DUPLICADO.
+
+    // return ResponseEntity.ok(actualizado); // aqui nos devuelve el exito con un
+    // codigo 200 http
     // }
 
     @DeleteMapping("/{rut}")
