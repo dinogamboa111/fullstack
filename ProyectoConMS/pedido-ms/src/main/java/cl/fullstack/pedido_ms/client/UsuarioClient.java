29f4a2bbb53acbc9ee6666f79fc45e4b0432eba3
camilaeri <cam.erices@duocuc.cl>
2025-06-20 16:05:20 -0400

    11

--- ProyectoConMS/pedido-ms/src/main/java/cl/fullstack/pedido_ms/client/UsuarioClient.java
+++ ProyectoConMS/pedido-ms/src/main/java/cl/fullstack/pedido_ms/client/UsuarioClient.java
@@ -1,5 +1,38 @@
 package cl.fullstack.pedido_ms.client;
 
-public class UsuarioClient {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import cl.fullstack.pedido_ms.dto.external.UsuarioDTO;
+
+import org.springframework.http.HttpMethod;
+
+//service marca esta clase como un componente de servicio de spring. asi se detecta automaticamente en el escaneo y se registra como bean
+@Service
+//esta clase ProductoClient se encarga de comunicarse con producto-ms
+public class UsuarioClient {
+    //aqui estamos inyectando automaticamente el restemplate q configuramos en RestTemplateConfig
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    //aqui hacemos una llamada get a la url 
+    public UsuarioDTO obtenerUsuariosById(int usuarioId) {
+        return restTemplate.getForObject("http://usuario-service/api/usuarios/" + usuarioId, UsuarioDTO.class);
+    }
+    //tambien hace una llamada get a la url, pero obtenienndo todos en una lista, se usa .exchange porque getForObjet no doporta tipos genericos como List<UsuarioDTO>
+    public List<UsuarioDTO> obtenerTodosLosUsuarios() {
+        ResponseEntity<List<UsuarioDTO>> response = restTemplate.exchange(
+            "http://usuario-service/api/usuarios/",
+            HttpMethod.GET,
+            null,
+            //este new le dice a spring que espere una lista de UusuarioDTO
+            new ParameterizedTypeReference<List<UsuarioDTO>>() {}
+        );
+        return response.getBody();
+    }
 }
--- ProyectoConMS/pedido-ms/src/main/java/cl/fullstack/pedido_ms/dto/PedidoDTO.java
+++ ProyectoConMS/pedido-ms/src/main/java/cl/fullstack/pedido_ms/dto/PedidoDTO.java
@@ -16,5 +16,6 @@ public class PedidoDTO {
     private int idComuna;
     private boolean estadoPedido;
     private List<DetallePedidoDTO> detallePedido;
+    private int usuarioId;
     
 }
